85be829b436843c4837566029a03671d8f7a6fa7
Alston <alston1399@gmail.com>
2026-08-18 20:02:55 -0700

    removed local folder

--- fish/completions/fish_logo.fish
+++
@@ -1,2 +0,0 @@
-# https://github.com/laughedelic/fish_logo/blob/master/completions/fish_logo.fish
-complete -c fish_logo --exclusive -a '(set_color --print-colors)'
--- fish/completions/quiet.fish
+++
@@ -1,2 +0,0 @@
-# my quiet command
-complete -c quiet -f -r -d "Run command silently" -a "(__fish_complete_subcommand)"
